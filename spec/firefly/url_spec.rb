7739278043f551b821930915b4b46514d2c6aaaf
Manu Cupcic <cupcicm@gmail.com>
2015-03-02 08:32:16 +0100

    [Refactor] Remove some duplicated code.
    Makes the calls to shorten a tiny bit simpler (and easier to change).

--- spec/firefly/url_spec.rb
+++ spec/firefly/url_spec.rb
@@ -2,76 +2,81 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "Url" do
 
+  def shorten(url = nil)
+    url ||= "http://example.com/"
+    Firefly::Url.shorten(url)
+  end
+
   describe "shortening" do
     it "should generate a code after create" do
-      url = Firefly::Url.shorten("http://example.com/")
+      url = shorten
       Firefly::Url.where(url: "http://example.com/").first.code.should_not be_nil
     end
 
     it "should set a clicks count of 0 for newly shortened urls" do
-      url = Firefly::Url.shorten("http://example.com/")
+      url = shorten
       Firefly::Url.where(url: "http://example.com/").first.clicks.should eql(0)
     end
 
     it "should create a new Firefly::Url with a new long_url" do
       lambda {
-        Firefly::Url.shorten("http://example.com/")
+        shorten
       }.should change(Firefly::Url, :count).by(1)
     end
 
     it "should return an existing Firefly::Url if the long_url exists" do
-      Firefly::Url.shorten("http://example.com/")
+      shorten
       lambda {
-        Firefly::Url.shorten("http://example.com/")
+        shorten
       }.should_not change(Firefly::Url, :count)
     end
 
     it "should normalize urls correctly" do
       # Note the trailing '/'
-      Firefly::Url.shorten("http://example.com/")
+      shorten
       lambda {
-        Firefly::Url.shorten("http://example.com")
+        shorten
       }.should_not change(Firefly::Url, :count)
     end
 
     it "should shortend urls containing spaces" do
       lambda {
-        url = Firefly::Url.shorten("http://example.com/article with spaces.html")
+        url = shorten("http://example.com/article with spaces.html")
       }.should change(Firefly::Url, :count).by(1)
     end
 
     it "should escape urls with spaces" do
-      url = Firefly::Url.shorten("http://example.com/article with spaces.html")
+      url = shorten("http://example.com/article with spaces.html")
       url.url.should eql("http://example.com/article%20with%20spaces.html")
     end
 
     it "should shortend urls containing weird characters" do
       lambda {
-        url = Firefly::Url.shorten("http://example.com/?a=\11\15")
+        url = shorten("http://example.com/?a=\11\15")
       }.should change(Firefly::Url, :count).by(1)
     end
 
     it "should escape urls with weird characters" do
-      url = Firefly::Url.shorten("http://example.com/?a=\11\15")
+      url = shorten("http://example.com/?a=\11\15")
       url.url.should eql("http://example.com/?a=%09%0D")
     end
 
     it "should not unescape invalid URL characters" do
-      url = Firefly::Url.shorten("http://example.com/?a=%09")
+      url = shorten("http://example.com/?a=%09")
       url.url.should eql("http://example.com/?a=%09")
     end
 
     it "should not escape already escaped URLs" do
-      url = Firefly::Url.shorten("http://en.wikipedia.org/wiki/Tarski%27s_circle-squaring_problem")
+      url = shorten("http://en.wikipedia.org/wiki/Tarski%27s_circle-squaring_problem")
       url.url.should eql("http://en.wikipedia.org/wiki/Tarski's_circle-squaring_problem")
     end
 
     it "should automatically forward code to prevent duplicates" do
-      url = Firefly::Url.shorten("http://example.com/")
+      url = shorten("http://example.com/")
       the_code = url.code.next
       Firefly::Url.create(url: "http://example.com/blah", code: the_code)
 
-      url_correct = Firefly::Url.shorten("http://example.com/testit")
+      url_correct = shorten("http://example.com/testit")
       url_correct.code.should_not eql(the_code)
       url_correct.code.should eql(the_code.next)
     end
@@ -85,7 +90,7 @@ describe "Url" do
       "http://ariejan.net:8080/"
     ].each do |url|
       it "should accept #{url}" do
-        Firefly::Url.shorten(url).should_not be_nil
+        shorten(url).should_not be_nil
       end
     end
 
@@ -96,7 +101,7 @@ describe "Url" do
     ].each do |url|
       it "should not accept #{url}" do
         lambda {
-          Firefly::Url.shorten(url).should be_nil
+          shorten(url).should be_nil
         }.should raise_error(Firefly::InvalidUrlError)
       end
     end
